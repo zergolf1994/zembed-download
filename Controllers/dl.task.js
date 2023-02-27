d57b52917ee5e85f5f8c72132e237620c1ace5a3
zergolf1994 <zergolf1994@gmail.com>
2023-02-27 23:54:58 +0700

    fix task resp

--- Controllers/dl.task.js
+++ Controllers/dl.task.js
@@ -1,8 +1,16 @@
 "use strict";
+const path = require("path");
+const fs = require("fs-extra");
 const { Task } = require(`../Utils`);
 
 module.exports = async (req, res) => {
   try {
+    
+    let file_task = path.join(global.dirPublic, "task.json");
+    if (!fs.existsSync(file_task)) {
+      return res.json({ status: "false", msg: "no_file_task" });
+    }
+
     let task = await Task();
     return res.json(task);
   } catch (error) {
